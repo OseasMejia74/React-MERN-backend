9f5f7c232d3a4a8c722f9004916d9a3cc0a518a0
Oseas <=>
2025-07-21 17:24:04 -0600

    Firt commit

---
+++ routes/events.js
@@ -0,0 +1,54 @@
+/*
+    Rutas de eEventos / Events
+    host + /api/events
+*/
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
+
+const router = Router();
+
+// Todas tienen que pasar por la validación de JWT
+router.use( validarJWT ); // Para que cualquier petición que se realice después valide el token
+
+// Obtener eventos
+router.get( '/', getEventos );
+
+// Crear un nuevo evento
+router.post(
+    '/', 
+    [
+        check('tittle', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+        check('end', 'Fecha de finalización es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    crearEvento);
+
+// Actualizar evento
+router.put(
+    '/:id',
+    [
+        check('tittle', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+        check('end', 'Fecha de finalización es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    actualizarEvento);
+
+// Borrar un evento
+router.delete( '/:id', eliminarEvento );
+
+module.exports = router;
+
+
+
+
+
+
+
